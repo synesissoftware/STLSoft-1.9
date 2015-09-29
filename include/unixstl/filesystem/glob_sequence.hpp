b3ffadbe6dbe9df6328a370f1d51e89de056dca7
Matt <matthew@synesis.com.au>
2015-09-30 06:41:34 +1000

    ~ Clang-compatibility

--- include/unixstl/filesystem/glob_sequence.hpp
+++ include/unixstl/filesystem/glob_sequence.hpp
@@ -4,7 +4,7 @@
  * Purpose:     glob_sequence class.
  *
  * Created:     15th January 2002
- * Updated:     26th March 2013
+ * Updated:     30th September 2015
  *
  * Thanks:      To Carlos Santander Bernal for helping with Mac compatibility.
  *              To Nevin Liber for pressing upon me the need to lead by
@@ -12,7 +12,7 @@
  *
  * Home:        http://stlsoft.org/
  *
- * Copyright (c) 2002-2013, Matthew Wilson and Synesis Software
+ * Copyright (c) 2002-2015, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -189,6 +189,8 @@
 # define GLOB_SEQUENCE_CTOR_PRIMARY_FORM
 #elif defined(STLSOFT_COMPILER_IS_BORLAND)
 # define GLOB_SEQUENCE_CTOR_OLD_FORM
+#elif defined(STLSOFT_COMPILER_IS_CLANG)
+# define GLOB_SEQUENCE_CTOR_PRIMARY_FORM
 #elif defined(STLSOFT_COMPILER_IS_COMO)
 # define GLOB_SEQUENCE_CTOR_PRIMARY_FORM
 #elif defined(STLSOFT_COMPILER_IS_DMC)
